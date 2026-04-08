a64f5ab6b982a09e4972a6f02587ddc7a55af787
dullsword <525649253@qq.com>
2026-04-08 18:50:48 +0800

    feat(icons): 更新图标文件，移除16px图标并使用48px图标

--- devtools.js
+++ devtools.js
@@ -1,5 +1,5 @@
 chrome.devtools.panels.create(
     chrome.i18n.getMessage('devtools_panel_title') || 'LocalStorage Inspector',
-    'icons/icon16.png',
+    'icons/icon48.png',
     'panel/panel.html'
 );
